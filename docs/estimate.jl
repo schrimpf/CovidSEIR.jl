fc4118a73cb8f49c16dcea0415f4c67c24c44620
Paul Schrimpf <paul.schrimpf@gmail.com>
2020-04-09 06:59:29 -0700

    Files that should have been committed earlier.

---
+++ docs/estimate.jl
@@ -0,0 +1,19 @@
+fileprefix = match(r"([a-zA-Z]+)_.+",ARGS[1]).captures[1]
+println(ARGS)
+println(fileprefix)
+using CovidSEIR, Turing, JLD2, Dates, DataFrames
+countrynames = Dict( "us" => "US",
+                     "china" => "China",
+                     "korea" => "Korea, South",
+                     "canada" => "Canada",
+                     "italy" => "Italy")
+                     
+jmddir = normpath(joinpath(dirname(Base.find_package("CovidSEIR")),"..","docs","jmd"))
+covdf = covidjhudata()
+shift =fileprefix == "china" ?  65 : 1
+dayt0 =  Dates.Date("2020-01-22")  - Dates.Day(shift)
+dat = CountryData(covdf, countrynames[fileprefix], shift)
+mdl = CovidSEIR.TimeVarying.countrymodel(dat, CovidSEIR.TimeVarying.defaultpriors())
+println("Sampling for $fileprefix , ie $(countrynames[fileprefix])")
+cc = Turing.psample(mdl, NUTS(0.65), 5000, 4)
+JLD2.@save "$(jmddir)/$(fileprefix)_tv_$(Dates.today()).jld2" cc dayt0
